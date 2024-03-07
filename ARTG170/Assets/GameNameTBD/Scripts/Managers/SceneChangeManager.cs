986eb8cb5da13c9aad01d4b22168d2fd98016c8f
Aaron-Vuong <81384460+Aaron-Vuong@users.noreply.github.com>
2024-03-07 14:57:25 -0800

    Adding initial SceneState framework and reverting gameScene

--- ARTG170/Assets/GameNameTBD/Scripts/Managers/SceneChangeManager.cs
+++ ARTG170/Assets/GameNameTBD/Scripts/Managers/SceneChangeManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,6 +12,8 @@ public class SceneChangeManager : MonoBehaviour
     [Header("Debug")]
     [SerializeField] private Logger _logger;
 
+    [Header("Scene State")]
+    [SerializeField] private Dictionary<Scene, SceneState> _sceneStates = new Dictionary<Scene, SceneState>();
     private class LoadingMonoBehavior: MonoBehaviour { }
     public enum Scene {
         MainGameScene,
@@ -23,9 +27,40 @@ public class SceneChangeManager : MonoBehaviour
 
     private static Action onLoaderCallback;
     private static AsyncOperation loadingAsyncOperation;
+    private static Scene getCurrentScene()
+    {
+        // Convert a UnityEngine.SceneManagement.Scene to our own Scene enum
+        // TODO: Probably a better way to organize scenes than this.
+        UnityEngine.SceneManagement.Scene scene = SceneManager.GetActiveScene();
+        string[] sceneNames = System.Enum.GetNames(typeof(Scene));
 
+        for (int i = 0; i < sceneNames.Length; i++)
+        {
+            if (scene.name == sceneNames[i])
+            {
+                Debug.Log($"Current Scene is {sceneNames[i]}");
+                return (Scene)System.Enum.Parse(typeof(Scene), sceneNames[i]);
+            }
+        }
+        Debug.Log("Scene NOT FOUND!");
+        return Scene.MainMenuScene;
+    }
+    private static void SaveSceneState()
+    {
+        Scene scene = getCurrentScene();
+        // Save off the positions of all objects labelled with Pickup or Mob.
+        return;
+    }
+
+    private static void ApplySceneState(Scene scene)
+    {
+        // If there is no SceneState for this scene, create one.
+        return;
+    }
     public static void Load(Scene scene)
     {
+        // Save off the current SceneState.
+        SaveSceneState();
         onLoaderCallback = () =>
         {
             GameObject loadingGameObject = new GameObject("Loading GameObject");
@@ -43,6 +78,8 @@ public class SceneChangeManager : MonoBehaviour
         {
             yield return null;
         }
+        // Apply Scene State to the scene.
+        ApplySceneState(scene);
     }
 
     public static float GetLoadingProgress()
